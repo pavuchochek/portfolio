26a1b33730fd8dda988a717337526afdafae0158
Sofia Gribanova <sofia.gribanova@etu.iut-tlse3.fr>
2025-01-04 13:56:28 +0100

    feat : presentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,11 @@ module.exports = {
     extend: {},
     colors: {
       white: "#FFFFFF",
+      black: "#000000",
       transparent: "transparent",
       blue_background: "#353E5D",
       gold: "#B29988",
+
     },
   },
   plugins: [],
