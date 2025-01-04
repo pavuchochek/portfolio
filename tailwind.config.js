26546b1da4a06386d09be596d41ff1605c3194fb
Sofia Gribanova <sofia.gribanova@etu.iut-tlse3.fr>
2025-01-04 13:14:01 +0100

    feat : init tailwind and header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      transparent: "transparent",
+      blue_background: "#353E5D",
+      gold: "#B29988",
+    },
+  },
+  plugins: [],
+}
+
