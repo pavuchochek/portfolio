64fad82fbb288392cb8ef26e6f182d498144e4df
Sofia Gribanova <sofia.gribanova@etu.iut-tlse3.fr>
2025-01-05 19:45:26 +0100

    feat : add redirections

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
       gold: "#B29988",
 
     },
+    animation: {
+      blink: 'blink 1s infinite',
+    },
+    keyframes: {
+      blink: {
+        '0%, 100%': { opacity: 1, boxShadow: '0 0 20px rgba(255, 255, 255, 0.7)' },
+        '50%': { opacity: 0.7, boxShadow: '0 0 30px rgba(255, 255, 255, 1)' },
+      },
+    },
   },
   plugins: [],
 }
